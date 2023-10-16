2ed416424465a44e75c79b42c7a94edfa3b80f30
Voltsuuu <npoudel2007@gmail.com>
2023-10-15 21:55:49 -0400

    done

---
+++ src/RectangleInfo.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class RectangleInfo {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double length;
+        double width;
+        double area;
+        double perimeter;
+        double hypotenuse;
+
+        System.out.println("Length of the triangle?");
+        do {
+            if (scan.hasNextDouble()) {
+
+                length = scan.nextDouble();
+                if (length > 0) {
+                    break;
+                }
+            }
+            scan.nextLine();
+            System.out.println("Please enter a valid input");
+        } while (true);
+
+        // Asking side 2
+        System.out.println("Width of the triangle?");
+        do {
+            if (scan.hasNextDouble()) {
+
+                width = scan.nextDouble();
+                if (width > 0) {
+                    break;
+                }
+            }
+            scan.nextLine();
+            System.out.println("Please enter a valid input");
+        } while (true);
+
+        // Calculations
+        area = length*width;
+        perimeter = (length*2) + (width*2);
+        double cSquared = Math.pow(length, 2) + Math.pow(width, 2);
+        hypotenuse = Math.sqrt(cSquared);
+
+        System.out.printf("%-7s %.2f","Length:", length);
+        System.out.printf("\n%-6s %.2f", "Width:", width);
+        System.out.printf("\n%-5s %.2f", "Area:", area);
+        System.out.printf("\n%-8s %.2f", "Perimeter", perimeter);
+        System.out.printf("\n%-9s %.2f", "Hypotenuse:", hypotenuse);
+    }
+}
